58253f52580c2a3d2b4277d03001e57974f6db29
Yuni223 <whddl0020@gmail.com>
2023-06-16 10:33:54 +0900

    대문페이지와 자기소개페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     # 작성자는 추후 작성예정
 
-    def __str__(self):
+    def __str__(self):  # 페이지 생성시 페이지 번호 매기는 함수
         return f'[{self.pk}]{self.title}'
 
-    def get_absolute_url(self):
+    def get_absolute_url(self): # 상세페이지 접속 시 번호를 찾아주는 함수
         return f'/blog/{self.pk}/'
